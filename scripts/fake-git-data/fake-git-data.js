14c3744861f883d50f78e92c76a46cd340fa0361
Walter Galvao <walterbgneto@gmail.com>
2024-09-20 17:36:14 -0300

    fix: CI + Git Data

--- scripts/fake-git-data/fake-git-data.js
+++ scripts/fake-git-data/fake-git-data.js
@@ -1,11 +1,15 @@
 const simpleGit = require("simple-git");
-const commits = require("./fake-github-data.json");
+const commits = require("./commits.json");
+const authors = require("./authors.json");
 const git = simpleGit();
 
 async function createCommitAndCheckoutBranch() {
   try {
     const commit = commits[new Date().getDay()];
+    const randomAuthor = authors[Math.floor(Math.random() * authors.length)];
 
+    await git.addConfig("user.email", randomAuthor.email, false, "local");
+    await git.addConfig("user.name", randomAuthor.name, false, "local");
     await git.checkoutLocalBranch(commit.branch);
     await git.commit(commit.msg, { "--allow-empty": null });
   } catch (err) {
