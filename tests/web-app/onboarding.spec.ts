32e7ceea4f3a0dd97ba7502eebd52430731f5877
Jane Smith <jane.smith@example.com>
2024-09-25 13:11:46 -0300

    feat: more tests

--- tests/web-app/onboarding.spec.ts
+++ tests/web-app/onboarding.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 import { random } from "../../utils";
 
 test.describe(
-  "@onboarding",
+  "@web-app",
   {
     annotation: {
       type: "owner",
